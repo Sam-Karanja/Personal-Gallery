ddb965fd83795d83dbdcfbb6681170be06b61147
Moringa School <karanjasam210@gmail.com>
2022-05-31 11:40:03 +0300

    add templates

--- gallery_app/models.py
+++ gallery_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from email.mime import image
+from django.db import models
+
 
 # Create your models here.
+class image(models.Model):
+
+    name = models.Charfield(max_length= 35)
+    description = models.Charfield(max_length = 200)
+
+
+class category(models.Model):
+    name = models.Charfield(max_length = 35)
+
+
+class location(models.Model):
+    name = models. Charfield(max_length = 35)
