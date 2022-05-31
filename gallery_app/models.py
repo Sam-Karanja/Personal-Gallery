922a2b9b1b3154a137d81fa4105839563b021117
Moringa School <karanjasam210@gmail.com>
2022-05-31 11:46:42 +0300

    create test for models

--- gallery_app/models.py
+++ gallery_app/models.py
@@ -1,18 +1,35 @@
 from django.db import models
-from email.mime import image
-from django.db import models
+
 
 
 # Create your models here.
-class image(models.Model):
-
-    name = models.Charfield(max_length= 35)
-    description = models.Charfield(max_length = 200)
-
 
 class category(models.Model):
     name = models.Charfield(max_length = 35)
 
+    def __str__(self):
+        return self.name
+
+    def save_cat(self):
+        self.save()
 
 class location(models.Model):
     name = models. Charfield(max_length = 35)
+
+    def __str__(self):
+        return self.name
+
+    def save_loc(self):
+        self.save()
+
+class Image(models.Model):
+    image = models.ImageField(upload_to ='photos/', blank=True)
+    name = models.CharField(max_length=35)
+    description=models.CharField(max_length=200)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
