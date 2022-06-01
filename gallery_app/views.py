9a29f2a5d3913b5c5086f22984ab13acb1ecc909
Moringa School <karanjasam210@gmail.com>
2022-06-01 09:20:26 +0300

    function to get location

--- gallery_app/views.py
+++ gallery_app/views.py
@@ -1,3 +1,4 @@
+from email import message
 from unicodedata import category
 from django.shortcuts import render
 
@@ -27,8 +28,15 @@ def search_results(request):
 def location_results(request):
     location = Location.objects.all()
     photos = Image.current_images(location)
-    return render(request,'all-photos/location.html', {"photos":photos, "location":location})
 
+    if 'photo' in request.GET and request.GET["photo"]:
+        search_location = request.GET.get("photo")
+        found_location = Image.find_by_location(search_location)
+        message = f"{search_location}"
+        return render(request,'all-photos/location.html', {"photos":photos,"location":location,"message":message,"photos":found_location})
+    else:
+        message = "No suce Location"
+        return render(request,'all-photos/location.html',{"message":message})
 
 def photo(request,photo_id):
     photo = Image.objects.get(id=photo_id)
